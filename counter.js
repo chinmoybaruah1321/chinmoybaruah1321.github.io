e93da8ffff024432a53382af4d5a0dcc935d1c6f
chinmoybaruah1321 <chinmoybaruah@protonmail.com>
2018-05-08 12:47:12 -0400

    first commit

---
+++ counter.js
@@ -0,0 +1,583 @@
+var txt;
+var counts = {};
+var keys = [];
+
+function preload() {
+  txt = loadStrings('trial.txt');
+}
+
+/*
+ * String method to remove stop words
+ *   Usage: string_variable.removeStopWords();
+ *   Output: The original String with stop words removed
+ */
+String.prototype.removeStopWords = function() {
+    var x;
+    var y;
+    var word;
+    var stop_word;
+    var regex_str;
+    var regex;
+    var cleansed_string = this.valueOf();
+    var stop_words = new Array(
+        'a',
+        'about',
+        'above',
+        'across',
+        'after',
+        'again',
+        'against',
+        'all',
+        'almost',
+        'alone',
+        'along',
+        'already',
+        'also',
+        'although',
+        'always',
+        'among',
+        'an',
+        'and',
+        'another',
+        'any',
+        'anybody',
+        'anyone',
+        'anything',
+        'anywhere',
+        'are',
+        'area',
+        'areas',
+        'around',
+        'as',
+        'ask',
+        'asked',
+        'asking',
+        'asks',
+        'at',
+        'away',
+        'b',
+        'back',
+        'backed',
+        'backing',
+        'backs',
+        'be',
+        'became',
+        'because',
+        'become',
+        'becomes',
+        'been',
+        'before',
+        'began',
+        'behind',
+        'being',
+        'beings',
+        'best',
+        'better',
+        'between',
+        'big',
+        'both',
+        'but',
+        'by',
+        'c',
+        'came',
+        'can',
+        'cannot',
+        'case',
+        'cases',
+        'certain',
+        'certainly',
+        'clear',
+        'clearly',
+        'come',
+        'could',
+        'd',
+        'did',
+        'differ',
+        'different',
+        'differently',
+        'do',
+        'does',
+        'done',
+        'down',
+        'down',
+        'downed',
+        'downing',
+        'downs',
+        'during',
+        'e',
+        'each',
+        'early',
+        'either',
+        'end',
+        'ended',
+        'ending',
+        'ends',
+        'enough',
+        'even',
+        'evenly',
+        'ever',
+        'every',
+        'everybody',
+        'everyone',
+        'everything',
+        'everywhere',
+        'f',
+        'face',
+        'faces',
+        'fact',
+        'facts',
+        'far',
+        'felt',
+        'few',
+        'find',
+        'finds',
+        'first',
+        'for',
+        'four',
+        'from',
+        'full',
+        'fully',
+        'further',
+        'furthered',
+        'furthering',
+        'furthers',
+        'g',
+        'gave',
+        'general',
+        'generally',
+        'get',
+        'gets',
+        'give',
+        'given',
+        'gives',
+        'go',
+        'going',
+        'good',
+        'goods',
+        'got',
+        'great',
+        'greater',
+        'greatest',
+        'group',
+        'grouped',
+        'grouping',
+        'groups',
+        'h',
+        'had',
+        'has',
+        'have',
+        'having',
+        'he',
+        'her',
+        'here',
+        'herself',
+        'high',
+        'high',
+        'high',
+        'higher',
+        'highest',
+        'him',
+        'himself',
+        'his',
+        'how',
+        'however',
+        'i',
+        'if',
+        'important',
+        'in',
+        'interest',
+        'interested',
+        'interesting',
+        'interests',
+        'into',
+        'is',
+        'it',
+        'its',
+        'itself',
+        'j',
+        'just',
+        'k',
+        'keep',
+        'keeps',
+        'kind',
+        'knew',
+        'know',
+        'known',
+        'knows',
+        'l',
+        'large',
+        'largely',
+        'last',
+        'later',
+        'latest',
+        'least',
+        'less',
+        'let',
+        'lets',
+        'like',
+        'likely',
+        'long',
+        'longer',
+        'longest',
+        'm',
+        'made',
+        'make',
+        'making',
+        'man',
+        'many',
+        'may',
+        'me',
+        'member',
+        'members',
+        'men',
+        'might',
+        'more',
+        'most',
+        'mostly',
+        'mr',
+        'mrs',
+        'much',
+        'must',
+        'my',
+        'myself',
+        'n',
+        'necessary',
+        'need',
+        'needed',
+        'needing',
+        'needs',
+        'never',
+        'new',
+        'new',
+        'newer',
+        'newest',
+        'next',
+        'no',
+        'nobody',
+        'non',
+        'noone',
+        'not',
+        'nothing',
+        'now',
+        'nowhere',
+        'number',
+        'numbers',
+        'o',
+        'of',
+        'off',
+        'often',
+        'old',
+        'older',
+        'oldest',
+        'on',
+        'once',
+        'one',
+        'only',
+        'open',
+        'opened',
+        'opening',
+        'opens',
+        'or',
+        'order',
+        'ordered',
+        'ordering',
+        'orders',
+        'other',
+        'others',
+        'our',
+        'out',
+        'over',
+        'p',
+        'part',
+        'parted',
+        'parting',
+        'parts',
+        'per',
+        'perhaps',
+        'place',
+        'places',
+        'point',
+        'pointed',
+        'pointing',
+        'points',
+        'possible',
+        'present',
+        'presented',
+        'presenting',
+        'presents',
+        'problem',
+        'problems',
+        'put',
+        'puts',
+        'q',
+        'quite',
+        'r',
+        'rather',
+        'really',
+        'right',
+        'right',
+        'room',
+        'rooms',
+        's',
+        'said',
+        'same',
+        'saw',
+        'say',
+        'says',
+        'second',
+        'seconds',
+        'see',
+        'seem',
+        'seemed',
+        'seeming',
+        'seems',
+        'sees',
+        'several',
+        'shall',
+        'she',
+        'should',
+        'show',
+        'showed',
+        'showing',
+        'shows',
+        'side',
+        'sides',
+        'since',
+        'small',
+        'smaller',
+        'smallest',
+        'so',
+        'some',
+        'somebody',
+        'someone',
+        'something',
+        'somewhere',
+        'state',
+        'states',
+        'still',
+        'still',
+        'such',
+        'sure',
+        't',
+        'take',
+        'taken',
+        'than',
+        'that',
+        'the',
+        'their',
+        'them',
+        'then',
+        'there',
+        'therefore',
+        'these',
+        'they',
+        'thing',
+        'things',
+        'think',
+        'thinks',
+        'this',
+        'those',
+        'though',
+        'thought',
+        'thoughts',
+        'three',
+        'through',
+        'thus',
+        'to',
+        'today',
+        'together',
+        'too',
+        'took',
+        'toward',
+        'turn',
+        'turned',
+        'turning',
+        'turns',
+        'two',
+        'u',
+        'under',
+        'until',
+        'up',
+        'upon',
+        'us',
+        'use',
+        'used',
+        'uses',
+        'v',
+        'very',
+        'w',
+        'want',
+        'wanted',
+        'wanting',
+        'wants',
+        'was',
+        'way',
+        'ways',
+        'we',
+        'well',
+        'wells',
+        'went',
+        'were',
+        'what',
+        'when',
+        'where',
+        'whether',
+        'which',
+        'while',
+        'who',
+        'whole',
+        'whose',
+        'why',
+        'will',
+        'with',
+        'within',
+        'without',
+        'work',
+        'worked',
+        'working',
+        'works',
+        'would',
+        'x',
+        'y',
+        'year',
+        'years',
+        'yet',
+        'you',
+        'young',
+        'younger',
+        'youngest',
+        'your',
+        'yours',
+        'z'
+    )
+         
+    // Split out all the individual words in the phrase
+    words = cleansed_string.match(/[^\s]+|\s+[^\s+]$/g)
+ 
+    // Review all the words
+    for(x=0; x < words.length; x++) {
+        // For each word, check all the stop words
+        for(y=0; y < stop_words.length; y++) {
+            // Get the current word
+            word = words[x].replace(/\s+|[^a-z]+/ig, "");   // Trim the word and remove non-alpha
+             
+            // Get the stop word
+            stop_word = stop_words[y];
+             
+            // If the word matches the stop word, remove it from the keywords
+            if(word.toLowerCase() == stop_word) {
+                // Build the regex
+                regex_str = "^\\s*"+stop_word+"\\s*$";      // Only word
+                regex_str += "|^\\s*"+stop_word+"\\s+";     // First word
+                regex_str += "|\\s+"+stop_word+"\\s*$";     // Last word
+                regex_str += "|\\s+"+stop_word+"\\s+";      // Word somewhere in the middle
+                regex = new RegExp(regex_str, "ig");
+             
+                // Remove the word from the keywords
+                cleansed_string = cleansed_string.replace(regex, " ");
+            }
+        }
+    }
+    return cleansed_string.replace(/^\s+|\s+$/g, "");
+}
+
+var listOfObjects = [];
+
+function setup() {
+  var allwords = txt.join("\n");
+  bar = allwords.removeStopWords();
+  //var tokens_before = allwords.split(/\W+/);
+  var tokens = bar.split(/\W+/);
+
+  for (var i = 0; i < tokens.length; i++) {
+    var word = tokens[i].toLowerCase();
+    if (!/\d+/.test(word)) {
+      //console.log(word);
+      if (counts[word] === undefined) {
+        counts[word] = 1;
+        keys.push(word);
+      } else {
+        counts[word] = counts[word] + 1;
+      }
+    }
+  }
+
+  keys.sort(compare);
+
+  function compare(a, b) {
+    var countA = counts[a];
+    var countB = counts[b];
+    return countB - countA;
+  }
+
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    //createDiv(key + " " + counts[key]);
+  }
+
+  for(var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      var singleObj = {};
+      singleObj['text'] = key;
+      singleObj['size'] = counts[key];
+      listOfObjects.push(singleObj);
+  }
+
+  var width = 1500;
+  var height = 1500;
+  var fill = d3.scale.category20();
+
+  d3.layout.cloud()
+      .size([width, height])
+      .words(listOfObjects)
+      .rotate(function() {
+          return ~~(Math.random() * 2) * 90;
+        })
+      .font("Impact")
+      .fontSize(function(d) {
+          return d.size;
+      })
+      .on("end", drawSkillCloud)
+      .start();
+
+  noCanvas();
+}
+
+function drawSkillCloud(words) {
+    d3.select("#cloud svg").remove();
+    d3.select("#cloud").append("svg")
+        .attr("width", 1024)
+        .attr("height", 1024)
+        .append("g")
+        .attr("transform", "translate(" + ~~(width / 2) + "," + ~~(height / 2) + ")")
+        .selectAll("text")
+        .data(words)
+        .enter().append("text")
+        .style("font-size", function(d) {
+            return d.size + "px";
+        })
+        .style("-webkit-touch-callout", "none")
+        .style("-webkit-user-select", "none")
+        .style("-khtml-user-select", "none")
+        .style("-moz-user-select", "none")
+        .style("-ms-user-select", "none")
+        .style("user-select", "none")
+        .style("cursor", "default")
+        .style("font-family", "Impact")
+        .style("fill", function(d, i) {
+            return fill(i);
+        })
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) {
+            return d.text;
+        });
+}
+
+var svg = document.getElementsByTagName("svg")[0];
+console.log(svg);
+var bbox = svg.getBBox();
+var viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ");
+svg.setAttribute("viewBox", viewBox);
